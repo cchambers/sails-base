57d33c81d6ce5ab7048c17775aedea79b359b716
Chris Chambers <cchambers@gmail.com>
2015-07-09 23:52:14 -0400

    login updates

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -11,21 +11,23 @@ module.exports = {
   login: function(req, res) {
     passport.authenticate('local', function(err, user, info) {
       if ((err) || (!user)) {
-       return res.send({
-         message: info.message,
-         user: user
-       });
-     }
-     req.logIn(user, function(err) {
-      if (err) res.send(err);
-        //console.log(req.headers);
-        return res.redirect(req.headers.referer);
+        return res.send({
+          message: info.message,
+          user: user
+        });
+      }
+      req.logIn(user, function(err) {
+        if (err) res.send(err);
+        return res.send({
+          message: info.message,
+          user: user
+        });
       });
-   })(req, res);
- },
+    })(req, res);
+  },
 
- logout: function(req, res) {
-  req.logout();
-  res.redirect('/');
-}
+  logout: function(req, res) {
+    req.logout();
+    res.redirect('/');
+  }
 };
