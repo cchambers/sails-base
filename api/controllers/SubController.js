1798e9a76e1966093b908a15a8563c1f947ba5d2
Douglas DeVicariis <kootg04@gmail.com>
2015-07-16 07:33:07 -0700

    Can start to edit subs.

--- api/controllers/SubController.js
+++ api/controllers/SubController.js
@@ -1,12 +1,20 @@
 module.exports = {
   
   new: function (req, res) {
-    data = null;
+    data = 'undefined';
     if ( typeof(req.user) == 'undefined' ) {
       return res.redirect('/');
     } else {
-      return res.view('new-sub', { user: req.user, data: data});
+      return res.view('new-sub', { user: req.user, data: data });
     }
+  },
+  
+  edit: function (req, res) {
+    data = 'undefined';
+    var util = require('util');
+
+    console.log(util.inspect(req, {showHidden: false, depth: null}));
+    return res.view('edit-sub', { user: req.user, data: data });
   }
 };
 
--- config/routes.js
+++ config/routes.js
@@ -19,5 +19,7 @@ module.exports.routes = {
   'get /sub/:sub': 'FrontpageController.listing',
   'get /sub/:sub/:slug': 'FrontpageController.single',
 
-  'get /user/:user': 'UserController.userProfile'
+  'get /user/:user': 'UserController.userProfile',
+  
+  'get /edit/:sub': 'SubController.edit'
 };
