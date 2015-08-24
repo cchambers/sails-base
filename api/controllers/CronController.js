5ec92d3795c098eede67707c871dcfe9337fff32
Chris Chambers <cchambers@gmail.com>
2015-08-24 17:39:10 -0400

    Emergency fix

--- api/controllers/CronController.js
+++ api/controllers/CronController.js
@@ -1,13 +1,14 @@
 var CronJob = require('cron').CronJob;
 var fetches = 0;
-console.log(sails);
-new CronJob('* 15 * * * *', function () {
-  fetches++;
-  sails.controllers.bot.get(fetches);
-}, null, true, 'America/Los_Angeles');
 
 
-new CronJob('* 7 * * * *', function () {
-  fetches++;
-  sails.controllers.bot.postRandom();
-}, null, true, 'America/Los_Angeles');
+// new CronJob('* 15 * * * *', function () {
+//   fetches++;
+//   sails.controllers.bot.get(fetches);
+// }, null, true, 'America/Los_Angeles');
+
+
+// new CronJob('* 7 * * * *', function () {
+//   fetches++;
+//   sails.controllers.bot.postRandom();
+// }, null, true, 'America/Los_Angeles');
