d804c99c1ea4b4c33fd8d14bc2faf5c7ad97a7d2
Chris Chambers <cchambers@gmail.com>
2015-08-28 01:43:30 -0400

    style tweak and restart of daboot

--- api/controllers/CronController.js
+++ api/controllers/CronController.js
@@ -2,13 +2,13 @@ var CronJob = require('cron').CronJob;
 var fetches = 0;
 
 
-// new CronJob('* 15 * * * *', function () {
-//   fetches++;
-//   sails.controllers.bot.get(fetches);
-// }, null, true, 'America/Los_Angeles');
+new CronJob('*/15 * * * *', function () {
+  fetches++;
+  sails.controllers.bot.get(fetches);
+}, null, true, 'America/Los_Angeles');
 
 
-// new CronJob('* 7 * * * *', function () {
-//   fetches++;
-//   sails.controllers.bot.postRandom();
-// }, null, true, 'America/Los_Angeles');
+new CronJob('*/4 * * * *', function () {
+  fetches++;
+  sails.controllers.bot.postRandom();
+}, null, true, 'America/Los_Angeles');
