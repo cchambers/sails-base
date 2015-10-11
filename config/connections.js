90ab7ee10f47495f3ee56b42e51ee714d30155e6
Chris Chambers <cchambers@gmail.com>
2015-10-11 14:31:08 -0400

    more

--- config/connections.js
+++ config/connections.js
@@ -5,19 +5,10 @@ module.exports.connections = {
 
   mongodb: {
     adapter: 'sails-mongo',
-    host: 'localhost',
+    host: 'gravy.io',
     port: 27017,
     user: 'site',
     password: '#YQLnjz#5Kv2',
     database: 'gravy'
-  },
-
-  mongotestdb: {
-    adapter: 'sails-mongo',
-    host: 'ds031193.mongolab.com',
-    port: 31193,
-    user: 'site',
-    password: 'tyFZ6G6k',
-    database: 'gravytesting'
   }
 };
